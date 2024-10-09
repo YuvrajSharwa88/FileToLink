a76e53ceb8e1a64716cc6dca15979110c1a13c16
YuvrajSharwa88 <yuvrajsarva6@gmail.com>
2024-10-09 18:23:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002366826693'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6893701094').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fapeno1079:QS4GmpJOKhfrHZYU@cluster0.9a5pu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
 # Shortlink Info
